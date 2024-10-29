9f550f923aa8aa6837f8e4dbcac200f49d75702f
mfchan1 <156487468+mfchan1@users.noreply.github.com>
2024-10-29 15:27:13 -0600

    Jenna shouldn't have trusted me

--- Project/app/src/main/java/com/example/king_of_the_castle_project/Entrant.java
+++
@@ -1,8 +0,0 @@
-package com.example.king_of_the_castle_project;
-
-public class Entrant extends User {
-    public Entrant(String name, String email, String phoneNumber, String Id) {
-        super(name, email, phoneNumber, Id);
-    }
-
-}

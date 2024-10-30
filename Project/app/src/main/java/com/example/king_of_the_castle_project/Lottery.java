24ce351037ca73581adbbb7dc8fb36c83ca39970
uglyduckling30 <120159510+uglyduckling30@users.noreply.github.com>
2024-10-30 11:31:48 -0600

    added javadocs to lottery and notification class

--- Project/app/src/main/java/com/example/king_of_the_castle_project/Lottery.java
+++ Project/app/src/main/java/com/example/king_of_the_castle_project/Lottery.java
@@ -3,8 +3,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-//this class relies on the EventDetails class, which holds the waiting list and the number of entrants wanted
 
+
+/**
+ * This is a class that defines the Lottery method to randomly choose x amount of entrants and add them to a list called selectedAttendees
+ */
 public class Lottery { ;
     private List<Entrant> selectedAttendees;
 
@@ -12,6 +15,11 @@ public class Lottery { ;
         this.selectedAttendees = new ArrayList<>(); //selectedAttendees will be the list of entrants we randomly get from the waiting list
     }
 
+    /**
+     * This selects x amount of random entrants from the waitingList (from EventDetails class)
+     * @param eventDetails
+     * this is the event object to use
+     */
     public void selectRandomEntrants(EventDetails eventDetails) { //EventDetails will hold the values of the list of entrants (waiting list) as well as the number of participants needed for the event
         int x = eventDetails.numberOfEntrants;  // Direct access to the private field
         List<Entrant> waitingList = eventDetails.waitingList;  // Direct access to the private field
@@ -26,6 +34,10 @@ public class Lottery { ;
         }
     }
 
+    /**
+     * Returns the selectedAttendees list
+     * @return
+     */
     public List<Entrant> getSelectedAttendees() {
         return selectedAttendees; //returns the selected Attendees
     }

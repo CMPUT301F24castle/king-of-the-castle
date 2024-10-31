b52043a21b124b114228dd07259d69b06bd645bb
Albert Huang <albert7@ualberta.ca>
2024-10-31 14:15:37 -0600

    Fixes to event class, implemented image browsing on create event, implemented confirmation messages

--- Project/app/src/main/java/com/example/king_of_the_castle_project/CreateEventActivity.java
+++ Project/app/src/main/java/com/example/king_of_the_castle_project/CreateEventActivity.java
@@ -1,15 +1,36 @@
 package com.example.king_of_the_castle_project;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
+import java.util.Locale;
+
 public class CreateEventActivity extends AppCompatActivity {
-    private boolean geolocation_check;
+    private ActivityResultLauncher<Intent> imageSelectorLauncher;
+    protected boolean geolocation_check = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,11 +55,53 @@ public class CreateEventActivity extends AppCompatActivity {
             geolocation_check = isChecked;
         });
 
+        // Image information retriever
+        imageSelectorLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        Uri imageUri = result.getData().getData();
+                    }
+                }
+        );
+
+        // event listener for selecting a photo
+        uploadImageButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Open image selector
+                imageSelector();
+                }
+        });
+
         // event listener for "create event"
         eventCreation.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Send information to firebase first
-                // TODO
+                // fetch info
+                String name = eventName.getText().toString();
+                String location = eventLocation.getText().toString();
+                String details = eventDetails.getText().toString();
+                String maxParticipants = eventMaxParticipants.getText().toString();
+                String date = eventDate.getText().toString();
+                String time = "12:00";
+                int number = 60; // Max participants for now
+                // Conversion of maxparticipants to int
+                try {
+                    number = Integer.parseInt(maxParticipants);
+                } catch (NumberFormatException e) {
+                    // Throw toast dialogue, return
+                    displayToastNotification("Invalid max participants, returning to organizer screen.");
+                    finish();
+                }
+                // Create QR code
+                // generateQRCode();
+                // Create empty waitlist
+                WaitList waitlist = new WaitList(number);
+                // Create event then send to firebase
+                Event newEvent = new Event(name, date, time, location, details, number, waitlist, geolocation_check);
+                // Passing data back
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("eventCreated", name);
+                setResult(RESULT_OK, resultIntent);
                 // return
                 finish();
             }
@@ -61,4 +124,24 @@ public class CreateEventActivity extends AppCompatActivity {
             return insets;
         });*/
     }
+
+    protected void imageSelector() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        imageSelectorLauncher.launch(Intent.createChooser(intent, "Select Picture"));
+    }
+
+    protected void displayToastNotification(String message) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_notification_layout, findViewById(R.id.custom_toast_container));
+        TextView text = layout.findViewById(R.id.toast_text);
+        text.setText(message);
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.setGravity(Gravity.BOTTOM, 0, 100);
+        toast.show();
+    }
+
 }
--- Project/app/src/main/java/com/example/king_of_the_castle_project/OrganizerActivity.java
+++ Project/app/src/main/java/com/example/king_of_the_castle_project/OrganizerActivity.java
@@ -2,12 +2,21 @@ package com.example.king_of_the_castle_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OrganizerActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> activityResultLauncher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +33,48 @@ public class OrganizerActivity extends AppCompatActivity {
             }
         });
 
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    Intent returnData = result.getData();
+                    if (returnData != null) {
+                        // String facilityName = returnData.getStringExtra("facilityModified");
+                        if (returnData.hasExtra("facilityModified")) { // mod facility notification
+                            LayoutInflater inflater = getLayoutInflater();
+                            View layout = inflater.inflate(R.layout.toast_notification_layout, findViewById(R.id.custom_toast_container));
+                            TextView text = layout.findViewById(R.id.toast_text);
+                            text.setText("Facility has been successfully modified");
+                            Toast toast = new Toast(getApplicationContext());
+                            toast.setDuration(Toast.LENGTH_SHORT);
+                            toast.setView(layout);
+                            toast.setGravity(Gravity.BOTTOM, 0, 100);
+                            toast.show();
+                            // Make sure it doesn't run again
+                            returnData.removeExtra("facilityModified");
+                        } else if (returnData.hasExtra("eventCreated")) { // Create event notification
+                            String eventName = returnData.getStringExtra("eventCreated");
+                            LayoutInflater inflater = getLayoutInflater();
+                            View layout = inflater.inflate(R.layout.toast_notification_layout, findViewById(R.id.custom_toast_container));
+                            TextView text = layout.findViewById(R.id.toast_text);
+                            text.setText(String.format("Event %s has been successfully created", eventName));
+                            Toast toast = new Toast(getApplicationContext());
+                            toast.setDuration(Toast.LENGTH_SHORT);
+                            toast.setView(layout);
+                            toast.setGravity(Gravity.BOTTOM, 0, 100);
+                            toast.show();
+                            // Make sure it doesn't run again
+                            returnData.removeExtra("eventCreated");
+                        }
+                        // for create event notification
+                    }
+                }
+        );
+
         // Clicking on manage facility goes to manage facility screen/activity
         manageFacilityButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(OrganizerActivity.this, ManageFacilityActivity.class);
-                startActivity(intent);
+                activityResultLauncher.launch(intent);
             }
         });
 
@@ -37,7 +83,7 @@ public class OrganizerActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(OrganizerActivity.this, CreateEventActivity.class);
-                startActivity(intent);
+                activityResultLauncher.launch(intent);
             }
         });
 

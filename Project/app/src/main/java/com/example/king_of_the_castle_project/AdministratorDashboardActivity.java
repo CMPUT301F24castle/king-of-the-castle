33a15a7f9cf144a5707db4b3f70013619f5efba9
Albert Huang <albert7@ualberta.ca>
2024-11-07 19:38:48 -0700

    keeping files up to date

--- Project/app/src/main/java/com/example/king_of_the_castle_project/AdministratorDashboardActivity.java
+++ Project/app/src/main/java/com/example/king_of_the_castle_project/AdministratorDashboardActivity.java
@@ -1,2 +1,40 @@
-package com.example.king_of_the_castle_project;public class AdministratorDashboardActivity {
+package com.example.king_of_the_castle_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdministratorDashboardActivity extends AppCompatActivity {
+    /**
+     * Default method for basic startup logic
+     * @param savedInstanceState
+     *      If there was an Instance saved, saved instances restores it
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.administrator_dashboard);
+
+        final Button browseEventsButton = findViewById(R.id.browse_event_button);
+        final Button browseProfilesButton = findViewById(R.id.browse_profile_button);
+        final Button browseImagesButton = findViewById(R.id.browse_images_button);
+        final Button changeRole = findViewById(R.id.change_role_button);
+
+        // change role should return to the choose role screen (where intent is called)
+        changeRole.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        browseEventsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(AdministratorDashboardActivity.this, browseEventsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
---
+++ Project/app/src/main/java/com/example/king_of_the_castle_project/browseEventsActivity.java
@@ -0,0 +1,75 @@
+package com.example.king_of_the_castle_project;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+
+public class browseEventsActivity extends AppCompatActivity {
+    /**
+     * Default method for basic startup logic
+     * @param savedInstanceState
+     *      If there was an Instance saved, saved instances restores it
+     */
+    private ListView listView;
+    private EventArrayAdapter arrayAdapter;
+    private ArrayList<Event> events;
+    private FirebaseFirestore db;
+
+    /**
+     * Default method for basic startup logic
+     * @param savedInstanceState
+     *      If there was an Instance saved, saved instances restores it
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // EdgeToEdge.enable(this);
+        setContentView(R.layout.administrator_manage_events_screen);
+        // Button
+        Button returnButton = findViewById(R.id.return_button);
+        // Initialize the firebase
+        db = FirebaseFirestore.getInstance();
+        // find listview
+        listView = findViewById(R.id.event_list);
+        // Initialize the events array
+        events = new ArrayList<>();
+        // Adapter
+        arrayAdapter = new EventArrayAdapter(this, events);
+        listView.setAdapter(arrayAdapter);
+        // Fetch events from firebase, then events.add
+        db.collection("events")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Event event = document.toObject(Event.class);
+                            events.add(event);
+                            arrayAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+
+        // Return button
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        /*
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });*/
+    }
+}

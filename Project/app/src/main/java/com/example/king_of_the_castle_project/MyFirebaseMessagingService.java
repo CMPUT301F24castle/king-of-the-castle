f250bebc7d043690a7216a64c6e8f84fc4cbf246
uglyduckling30 <120159510+uglyduckling30@users.noreply.github.com>
2024-11-30 18:00:31 -0700

    fixed notifs

---
+++ Project/app/src/main/java/com/example/king_of_the_castle_project/MyFirebaseMessagingService.java
@@ -0,0 +1,12 @@
+package com.example.king_of_the_castle_project;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirebaseMessagingService extends FirebaseMessagingService {
+}
